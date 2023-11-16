15ee031b1b076a4b770ef7c2676cc5f24c64bac8
AliBikini <ali.biyikli.98@outlook.com>
2023-11-16 17:37:22 +0100

    Worked on userlist page

--- apps/motard-angular/src/app/app.routes.ts
+++ apps/motard-angular/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Route } from '@angular/router';
+import { AboutComponent } from 'libs/frontend/frontend-angular/src/lib/about/about.component';
+import { UserListComponent } from 'libs/frontend/frontend-angular/src/lib/user/user-list/user-list.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = 
+[
+    { // lazyLoading child routes
+        path: 'frontend-angular',
+        loadChildren : () =>
+        import('@cswf-abiyikli-23/frontend/frontend-angular').then(
+            (esModule) => esModule.FrontendAngularModule
+        )
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'user',
+        component: UserListComponent
+    }
+];
--- libs/frontend/frontend-angular/src/lib/frontend-angular.module.ts
+++ libs/frontend/frontend-angular/src/lib/frontend-angular.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterLink, RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserService } from './user/user.service';
@@ -10,19 +10,24 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'about',
+    pathMatch: 'full',
+    component: AboutComponent,
+  },
+  {
+    path: 'user',
     pathMatch: 'full',
     component: UserListComponent,
   },
   {
-    path: '/:id',
+    path: 'user/:id',
     pathMatch: 'full',
     component: UserDetailComponent,
   },
 ];
 
 @NgModule({
-  imports: [CommonModule, HttpClientModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, HttpClientModule, RouterLink, RouterModule.forChild(routes)],
   declarations: [
     NavbarComponent,
     AboutComponent,
